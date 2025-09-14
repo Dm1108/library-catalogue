91055f2dab01b64392e36bba290337bb886ae13a
Dm1108 <bar.dm.leo@gmail.com>
2025-09-14 20:56:41 +0300

    changed Library method removeByAuthor()

--- src/main/java/school/sorokin/javacore/Library.java
+++ src/main/java/school/sorokin/javacore/Library.java
@@ -1,7 +1,6 @@
 package school.sorokin.javacore;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class Library {
@@ -32,12 +31,6 @@ public class Library {
     }
 
     public void removeByAuthor(String author) {
-        Iterator<Publication> pubIterator = publications.iterator();
-        while (pubIterator.hasNext()) {
-            Publication nextPub = pubIterator.next();
-            if (nextPub.getAuthor().equalsIgnoreCase(author)) {
-                pubIterator.remove();
-            }
-        }
+        publications.removeIf(pub -> pub.getAuthor().equalsIgnoreCase(author));
     }
 }
